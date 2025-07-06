cb64b7a84e25c8a509b9dbb4b84cc3780925d96d
ahyeon <aha9654@gmail.com>
2025-07-06 15:55:09 +0900

    Revert "Develop"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,15 +99,6 @@ const config = {
         contextualSearch: true,
       },
     }),
-
-  customFields: {
-    firebaseConfig: {
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID
-    }
-  }
 };
 
 export default config;
